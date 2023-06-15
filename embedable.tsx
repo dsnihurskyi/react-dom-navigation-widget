3cdc77a2e9ad17484e982f322190ffdd41213d69
John Snihurskiy <d.snihurskyi@do-it.co>
2023-06-15 20:10:00 +0300

    removed unnessessary div element appending

--- embedable.tsx
+++ embedable.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import EmbedWidget from './src/EmbedDomWidget';
 
 export const init = ({ isDefaultVisible }: { isDefaultVisible?: boolean}) => {
-  const root = document.createElement('div');
-  root.id = 'embed-widget-root';
-  document.body.appendChild(root);
-
   ReactDOM.render(
     <React.StrictMode>
       <EmbedWidget isDefaultVisible={Boolean(isDefaultVisible)} />
     </React.StrictMode>,
-    document.getElementById('embed-widget-root')
+    document.body
   );
 }
